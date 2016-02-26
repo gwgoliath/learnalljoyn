cca452526b943ce37ea60917d23f0a57031104b7
Goliath Peng <goliathpeng@gmail.com>
2016-02-26 13:47:09 +0800

    delete goliath_arm.sh

--- alljoyn/goliath_arm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scons BINDINGS=cpp WS=off BT=off ICE=off CRYPTO=builtin CPU=arm CROSS_COMPILE=/opt/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf- SERVICES="about,notification,controlpanel,config,onboarding,sample_apps"
